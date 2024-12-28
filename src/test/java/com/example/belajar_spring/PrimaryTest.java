c016b0693183f01b374fd394135af11e8e05afa2
puspakirana <puspakiranastv@gmail.com>
2024-12-28 14:01:34 +0700

    Primary Bean

---
+++ src/test/java/com/example/belajar_spring/PrimaryTest.java
@@ -0,0 +1,25 @@
+package com.example.belajar_spring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class PrimaryTest {
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(PrimaryConfiguration.class);
+    }
+
+    @Test
+    void testGetBean() {
+        Foo foo = applicationContext.getBean(Foo.class);
+        Foo foo1 = applicationContext.getBean("foo1", Foo.class);
+        Foo foo2 = applicationContext.getBean("foo2", Foo.class);
+        Assertions.assertSame(foo, foo1);
+        Assertions.assertNotSame(foo, foo2);
+    }
+}
