c0fb291ffb9727ea88f78d8bf4ce8a8fe9009a70
puspakirana <puspakiranastv@gmail.com>
2024-12-28 17:21:28 +0700

    Custom Scope

--- src/main/java/com/example/belajar_spring/ScopeConfiguration.java
+++ src/main/java/com/example/belajar_spring/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.example.belajar_spring;
 
+import com.example.belajar_spring.model.Bar;
 import com.example.belajar_spring.model.Foo;
+import com.example.belajar_spring.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -16,4 +19,19 @@ public class ScopeConfiguration {
         log.info("Create new foo");
         return new Foo();
     }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean()
+    @Scope("doubleton")
+    public Bar bar() {
+        log.info("Create new bar");
+        return new Bar();
+    }
+
 }
