e2889b9bda297977e8c6ba10e17c8b722f4077e6
puspakirana <puspakiranastv@gmail.com>
2024-12-28 15:52:06 +0700

    Circular Dependency

---
+++ src/main/java/com/example/belajar_spring/CyclicConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.belajar_spring;
+
+import com.example.belajar_spring.cyclic.CyclicA;
+import com.example.belajar_spring.cyclic.CyclicB;
+import com.example.belajar_spring.cyclic.CyclicC;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CyclicConfiguration {
+
+    @Bean
+    public CyclicA cyclicA(CyclicB cyclicB) {
+        return new CyclicA(cyclicB);
+    }
+
+    @Bean
+    public CyclicB cyclicB(CyclicC cyclicC) {
+        return new CyclicB(cyclicC);
+    }
+
+    @Bean
+    public CyclicC cyclicC(CyclicA cyclicA) {
+        return new CyclicC(cyclicA);
+    }
+
+}
---
+++ src/test/java/com/example/belajar_spring/CyclicTest.java
@@ -0,0 +1,17 @@
+package com.example.belajar_spring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CyclicTest {
+
+    @Test
+    void testCyclic() {
+        Assertions.assertThrows(Throwable.class, () -> {
+            ApplicationContext context = new AnnotationConfigApplicationContext(CyclicConfiguration.class);
+        });
+    }
+
+}
