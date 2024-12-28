998067a417ecb21fd5ef55f4ba653250d4b98978
puspakirana <puspakiranastv@gmail.com>
2024-12-28 16:31:03 +0700

    lazy bean

--- src/test/java/com/example/belajar_spring/DependsOnTest.java
+++ src/test/java/com/example/belajar_spring/DependsOnTest.java
@@ -1,5 +1,6 @@
 package com.example.belajar_spring;
 
+import com.example.belajar_spring.model.Foo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -11,12 +12,12 @@ public class DependsOnTest {
 
     @BeforeEach
     void setUp() {
-        applicationContext = new AnnotationConfigApplicationContext(DependsOnTest.class);
+        applicationContext = new AnnotationConfigApplicationContext(DependsOnConfiguration.class);
     }
 
     @Test
     void dependsOnTest() {
-
+        Foo foo = applicationContext.getBean(Foo.class);
     }
 
 }
