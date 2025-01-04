58d38b82b23362847d5ce27b8a423450eff6c12b
puspakirana <puspakiranastv@gmail.com>
2025-01-04 17:33:44 +0700

    Bean Factory

---
+++ src/test/java/com/example/belajar_spring/BeanFactoryTest.java
@@ -0,0 +1,34 @@
+package com.example.belajar_spring;
+
+import com.example.belajar_spring.model.Foo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BeanFactoryTest {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(ScanConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testBeanFactory() {
+        ObjectProvider<Foo> fooObjectProvider = applicationContext.getBeanProvider(Foo.class);
+        List<Foo> fooList = fooObjectProvider.stream().collect(Collectors.toList());
+
+        System.out.println(fooList.size());
+
+        Map<String, Foo> beans = applicationContext.getBeansOfType(Foo.class);
+        System.out.println(beans);
+    }
+}
