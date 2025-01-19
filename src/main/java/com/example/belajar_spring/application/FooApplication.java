8f0a91690e2aef442808ed1bf503c93790efc055
puspakirana <puspakiranastv@gmail.com>
2025-01-19 13:01:50 +0700

    Spring boot application

---
+++ src/main/java/com/example/belajar_spring/application/FooApplication.java
@@ -0,0 +1,23 @@
+package com.example.belajar_spring.application;
+
+import com.example.belajar_spring.model.Foo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FooApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+        Foo foo = applicationContext.getBean(Foo.class);
+        System.out.println(foo);
+    }
+
+    @Bean
+    public Foo foo() {
+        return new Foo();
+    }
+
+}
---
+++ src/test/java/com/example/belajar_spring/FooApplicationTest.java
@@ -0,0 +1,21 @@
+package com.example.belajar_spring;
+
+import com.example.belajar_spring.application.FooApplication;
+import com.example.belajar_spring.model.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FooApplication.class)
+public class FooApplicationTest {
+
+    @Autowired
+    Foo foo;
+
+    @Test
+    void testSpringBoot() {
+        Assertions.assertNotNull(foo);
+    }
+
+}
