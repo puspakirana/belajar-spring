5583c5b1b30c6e1890a739fd98b12928f954c03b
puspakirana <puspakiranastv@gmail.com>
2025-01-27 15:54:57 +0700

    Custimizing Spring Application

--- src/main/java/com/example/belajar_spring/application/FooApplication.java
+++ src/main/java/com/example/belajar_spring/application/FooApplication.java
@@ -2,6 +2,7 @@ package com.example.belajar_spring.application;
 
 import com.example.belajar_spring.model.Bar;
 import com.example.belajar_spring.model.Foo;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,8 +11,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class FooApplication {
 
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+//        Foo foo = applicationContext.getBean(Foo.class);
+//        System.out.println(foo);
+//    }
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(FooApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(FooApplication.class);
+//        springApplication.setBannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext applicationContext = springApplication.run(args);
+
         Foo foo = applicationContext.getBean(Foo.class);
         System.out.println(foo);
     }
